0d2ca8979e8a4fcfd7db1fd4599f4b4a42bd529e
AyenaCode <ayenacode1@gmail.com>
2024-10-01 10:10:26 -0400

    1

--- main.js
+++ main.js
@@ -1,2 +1,47 @@
-import "./src/settings";
+import { createElement } from "./src/dom";
 import "./style.css";
+
+const addList = document.getElementById("form");
+const markList = document.getElementById("markList");
+const deleteList = document.getElementById("deleteList");
+
+const UlList = document.getElementById("list");
+const tabLists = [];
+
+addList.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const forData = new FormData(addList);
+  const textareaValue = forData.get("textarea").toString().trim();
+
+  if (textareaValue === "") {
+    return;
+  }
+  tabLists.push(textareaValue);
+  addList.reset();
+  listItems();
+});
+
+console.log(tabLists);
+
+function listItems() {
+  if (tabLists.length > 0) {
+    tabLists.forEach((element) => {
+      const li = createElement("li", {
+        class:
+          "flex gap-4 my-2 border border-gray-100 rounded-md justify-between",
+      });
+      li.innerHTML = `<p class="text-left">
+            Lorem ipsum dolor, sit amet consectetur ${element}
+          </p>
+          <div class="flex flex-col gap-1">
+            <button class="btn rounded-none btn-success w-full" id="markList">
+              Terminer
+            </button>
+            <button class="btn rounded-none btn-primary w-full" id="deleteList">
+              Supprimer
+            </button>
+          </div>`;
+      UlList.appendChild(li);
+    });
+  }
+}
